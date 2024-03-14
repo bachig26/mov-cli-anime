8bda7b5804f518003fd7ec28a24f28e18c637d8c
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-03-14 18:06:24 +0000

    fix, feat: breaking before page is complete and yield should be used for faster search results

--- mov_cli_anime/anitaku/scraper.py
+++ mov_cli_anime/anitaku/scraper.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import List, Optional, Dict
+    from typing import List, Optional, Dict, Generator, Any
 
     from mov_cli import Config
     from mov_cli.http_client import HTTPClient
@@ -21,7 +21,7 @@ class AnitakuScraper(Scraper):
         self.base_url = "https://anitaku.to"
         super().__init__(config, http_client)
 
-    def search(self, query: str, limit: int = 10) -> List[Metadata]:
+    def search(self, query: str, limit: int = 20) -> List[Metadata]:
         query = query.replace(' ', '-')
         results = self.__results(query, limit)
         return results
@@ -59,9 +59,9 @@ class AnitakuScraper(Scraper):
             subtitles = None
         )
 
-    def __results(self, query: str, limit: int = None) -> List[Metadata]:
-        metadata_list = []
+    def __results(self, query: str, limit: int) -> Generator[Metadata, Any, None]:
         pagination = 1
+        result_count = 0
 
         while True:
             req = self.http_client.get(f"{self.base_url}/search.html?keyword={query}&page={pagination}")
@@ -96,29 +96,27 @@ class AnitakuScraper(Scraper):
 
                 genres = _p[3].findAll("a")
 
-                metadata_list.append(
-                    Metadata(
-                        id = id,
-                        title = title,
-                        type = type,
-                        year = year,
+                yield Metadata(
+                    id = id,
+                    title = title,
+                    type = type,
+                    year = year,
 
-                        extra_func = lambda: ExtraMetadata(
-                            description = [str.strip(x) for x in _p[2].strings if str.strip(x) != ''][1].replace(r"\r\n", "\r\n"),
-                            image_url = item.find("img")["src"],
-                            alternate_titles = [],
-                            cast = None,
-                            genres = [i.text.split(" ")[-1] for i in genres]
-                        )
+                    extra_func = lambda: ExtraMetadata(
+                        description = [str.strip(x) for x in _p[2].strings if str.strip(x) != ''][1].replace(r"\r\n", "\r\n"),
+                        image_url = item.find("img")["src"],
+                        alternate_titles = [],
+                        cast = None,
+                        genres = [i.text.split(" ")[-1] for i in genres]
                     )
                 )
 
-                if len(metadata_list) == limit:
-                    break
+                result_count += 1
+
+                if result_count >= limit:
+                    return None
 
             pagination += 1
-
-        return metadata_list
 
     def scrape_metadata_episodes(self, metadata: Metadata) -> Dict[int, int]:
         page = self.http_client.get(f"{self.base_url}/category/{metadata.id}")
@@ -141,14 +139,14 @@ class AnitakuScraper(Scraper):
         except Exception as e:
             self.logger.error(e)
             return None
-        
+
         urlh = f"https://dood.to{pass_md5}"
         res = self.http_client.get(urlh, headers = {"referer": "https://dood.to"}).text
         md5 = pass_md5.split("/")
         true_url = res + "MovCli3oPi?token=" + md5[-1]
 
         return true_url
-    
+
     def __streamwish(self, url):
         req = self.http_client.get(url).text
         file = re.findall(r'file:"(.*?)"', req)[0]
